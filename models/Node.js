5dcad56f267da7623101ede15e7ae8f61b63af59
Bnoii <138210692+Bnoii@users.noreply.github.com>
2025-11-11 16:06:00 +0530

    Update Node.js

--- models/Node.js
+++ models/Node.js
@@ -1,11 +1,14 @@
+// models/Node.js
 import mongoose from "mongoose";
 
 const NodeSchema = new mongoose.Schema({
-  siteId: { type: String, unique: true },
-  version: String,
-  url: String,
-  uptime: String,
+  siteId: { type: String, required: true, unique: true },
+  name: { type: String, default: "Unnamed Node" },
   nodeType: { type: String, default: "standard" },
+  version: { type: String },
+  url: { type: String },
+  uptime: { type: Number },
+  status: { type: String, default: "new" },
   lastSeen: { type: Date, default: Date.now },
 });
 
